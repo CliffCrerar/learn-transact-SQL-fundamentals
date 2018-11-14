84163ce4b9d70b23e8e42a16baa589e7faf0212b
Cliff Crerar <cliff.crerar@gmail.com>
2018-11-14 02:04:01 +0200

    Research into tech doc content

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/**
+ * JS SCRIPT FILE
+ * @summary Main and only script file for this project
+ * @author Cliff Crerar
+ * Created at     : 2018-11-13 23:27:13
+ * Last modified  : 2018-11-14 02:00:29
+ */
+
