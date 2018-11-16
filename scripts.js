96feceafd3c6e9c3190dfe3b8c8c5921db6f526e
Cliff Crerar <cliff.crerar@gmail.com>
2018-11-16 11:49:05 +0200

    Lekke

--- scripts.js
+++
@@ -1,8 +0,0 @@
-/**
- * JS SCRIPT FILE
- * @summary Main and only script file for this project
- * @author Cliff Crerar
- * Created at     : 2018-11-13 23:27:13
- * Last modified  : 2018-11-14 02:00:29
- */
-
