6dc53a62ed53e8f6604ce604d7c05f37a2e412d1
Cliff <cliff.crerar@gmail.com>
2019-05-19 21:37:17 +0200

    Yeas

--- _test_queries/ffc_sql_tech_doc_demodata.sql
+++ _test_queries/ffc_sql_tech_doc_demodata.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE Northwind;
-GO
-USE Northwind;
-GO
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
+GO                                                 
+USE Northwind;                                     
+GO                                                 
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;        
+/*!40103 SET TIME_ZONE='+00:00' */;                
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO,MSSQL' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+                                                   
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE categories (
+/*!40101 SET character_set_client = utf8 */;       
+CREATE TABLE categories (                          
   CategoryID int  NOT NULL,
   CategoryName varchar(15) NOT NULL,
   Description text,
