ee8a3fadf6d2804838b3eafe38938d424a8cd1bf
Cliff <cliff.crerar@gmail.com>
2019-05-19 22:12:07 +0200

    passed 15 tests

---
+++ scripts/_test.js
@@ -0,0 +1,6 @@
+(function() {
+  var NavLinks = document.getElementsByClassName('nav-link');
+  console.log('NavLinks: ', NavLinks);
+  var MainSections = document.getElementsByClassName('main-section');
+  console.log('MainSections: ', MainSections);
+}())
