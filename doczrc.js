a340f5625e0e26e567342c6c9de055d4e6d858dd
Cliff <cliff.crerar@gmail.com>
2019-06-18 05:03:45 +0200

    setting up new docs"

--- doczrc.js
+++ doczrc.js
@@ -1,18 +1,17 @@
-import { myCoolPlugin } from 'docz-plugin-mycoolplugin'
+//import { myCoolPlugin } from 'docz-plugin-mycoolplugin'
 
 export default {
   title: 'My Cool Project',
   description: 'This is my awesome documentation',
   theme: 'my-coolest-theme',
   themeConfig: {
-    colors: {
-      primary: 'tomato',
-    },
+    mode: 'dark',
   },
   modifyBundlerConfig: config => {
     /* do your magic here */
   },
-  plugins: [
-    myCoolPlugin()
-  ]
+  plugins: [],
+  export default {
+  dest: '/dist'
+    }
 }
