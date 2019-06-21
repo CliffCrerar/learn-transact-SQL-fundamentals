d034bfba4d1bb946f2cda811093e9d10cc817efb
Clifford Crerar <Clifford.Crerar@SurTech.co.za>
2019-06-21 22:47:27 +0200

    Added and refined content, still lots to add, added missing demo page

--- src/components/QuestionSnippet/index.jsx
+++
@@ -1,9 +0,0 @@
-import MnextMarkdown from '../services'
-
-const QuestionSnippet = (props) =>{
-    <div>
-        <MnextMarkdown source={props.text}/>
-    </div>
-}
-
-export default QuestionSnippet;
