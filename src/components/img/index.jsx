e62d4000246f54de3d789c33cd4b778c54fd6a98
CliffCrerar <cliff.crerar@gmail.com>
2019-11-13 12:38:54 +0200

    Fixing page

--- src/components/img/index.jsx
+++ src/components/img/index.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 
-//export default ()=> <img src="https://zeit-optipng-server-01.c1i44.now.sh/learnsql/cloudsql-server.png" alt="tsql image"/>
-
-
-
-// const QuoraImg = () =>
-    // <img
-        // src="//qsf.fs.quoracdn.net-3-images.logo.wordmark_default.svg-26-bfa6b94bc0d6af2e.svg"
-        // alt="Quora.com" />
-
-
-// export { QuoraImg };
+export default ({src, alt,divClass, imgClass,InjectElement})=>{
+	const classes = {
+		div:{
+			display: 'inline-block',
+			width: '100%'
+		},
+		img:{
+			display: 'block',
+			maxWidth: "100%",
+			height: "auto"
+		}
+	},
+	Injected = ()=>{
+		// InjectElement
+		//console.log('InjectElement: ', InjectElement);
+		if(InjectElement){
+			return <InjectElement/>
+		}
+		return (
+			<Fragment/>
+		)
+	}
+	imgClass ? console.log('not u') : console.log('u');
+	// console.log('imgClass: ', imgClass);
+	return (
+	<div style={divClass?divClass:classes.div}>
+		<img style={imgClass?imgClass:classes.img} src={src} alt={alt}/>
+		<Injected/>
+	</div>
+	)
+}
---
+++ src/pages/006-concepts/components/UserStory.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import {themeConfig} from '../../../../docz.config'
+export default () => {
+	return (
+		<React.Fragment>
+
+
+			<div className="user-story">
+				<style jsx='true'>{`
+    .user-story{
+        background-color: ${themeConfig.colors.secondary};
+        padding: 3vw;
+        font-size: 1.2em;
+    }
+    .user-story hr{
+        margin: 0px;
+        border-bottom: 1px solid black;
+    }
+    .user-story span {
+        text-align: justify !important;
+    }
+    .user-story li{
+        font-size: 0.7em;
+    }
+    .user-story-heading{
+        font-weight: bolder;
+        font-size: 1.5em;
+    }
+    blockquote h3 {
+        margin: 0 !important;
+    }
+`}</style>
+				<span className="user-story-heading">Motor Vehicle Traders Ltd</span><br />
+				<hr />
+				<span>
+					A used car sales business named <b>123 Sold (pty) ltd</b>, has many departments. Each department is managed by one person, this person, appointed as manager can only be in charge of one department.
+			<sup className="circ-num">&#10112;</sup>
+				</span><br /><br />
+				<span>
+					In the sales department, a salesman can advertise, and market any car. Any car may also be advertised by any salesman.
+			<sup className="circ-num">&#10113;</sup>
+				</span><br /><br />
+				<span>
+					<b>123 Sold (pty) ltd</b> is the holding company of a body shop next door, named <b>So Custom Body and Aftermarket Parts (pty) Ltd</b>. The body shop uses many different colours and shades, and in many cases cars are painted with different colours.
+			<sup className="circ-num">&#10114;</sup>
+				</span><br /><br />
+				<span>
+					A car always will have a base t1-color, but its panels can be painted in different colors, for example. The bonnet of the car be yellow, the roof can be green and trunk lid can be orange.
+			<sup className="circ-num">&#10115;</sup>
+				</span><br /><br />
+				<span>
+					So Custom Body and Aftermarket Parts must keep track of which colors are associated with which cars.
+			<sup className="circ-num"></sup>
+				</span><br /><br />
+				<li>
+					NOTE that the paragraphs are each numbered, this is for a reason which will become clear further down.
+		</li>
+			</div>
+		</React.Fragment>
+	)
+
+
+}
