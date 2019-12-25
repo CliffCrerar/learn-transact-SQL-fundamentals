de22681fd3ed6129d3ccbc91bae31b402dcded8d
Cliff Crerar <cliff@gmai.com>
2019-12-25 09:18:17 +0200

    starting the hype

--- src/components/embeded-sql-editor/index.js
+++ src/components/embeded-sql-editor/index.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
- class SQLEditor extends Component {
-	constructor(props){
+import React, { Component } from 'react';
+class SQLEditor extends Component {
+	constructor(props) {
 		super(props)
-		this.state = {width: '100%', height: '100%'}
+		this.state = { width: '100%', height: '100%' }
 	}
 
 	render() {
 		return (
-			<div style={{height: '1000px'}}>
-				<iframe is="x-frame-bypass" style={this.state} src="https://sqliteonline.com/#fiddle-5e030401b775emzkk4kxglzg"></iframe>
+			<div style={ { height: '1000px' } }>
+				<iframe style={ this.state }
+					src-doc="https://www.jdoodle.com/embed/v0/1M5O"
+					src="https://www.jdoodle.com/embed/v0/1M5O"
+					contentEditable="true"
+					frameborder="0"></iframe>
 			</div>
 		)
 	}
